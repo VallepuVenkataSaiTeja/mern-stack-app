6799e442f5810a4aeac8f3f14fbe85d16b1f3c8a
VallepuVenkataSaiTeja <152514721+VallepuVenkataSaiTeja@users.noreply.github.com>
2026-05-02 09:20:59 +0000

    fixed import issue

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,12 +11,21 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      importPlugin.flatConfigs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      'import/resolver': {
+        node: {},
+      },
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
